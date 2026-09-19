0268cedff4779b48362dd8dbedf7e2bba6572db8
aniketsawantcmaug26-create <aniket.sawant.cmaug26@gmail.com>
2026-09-19 16:24:41 +0530

    Completed

--- Java Assignment/Assignment 5(String)/Main.java
+++ Java Assignment/Assignment 5(String)/Main.java
@@ -3,12 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//	Problem1.problem1();
-	    //		Problem2.Problem2();
-		//	Problem3.Problem3();
-		//Problem4.Problem4();
-	// 	Problem5.Problem5();
-		//Problem6.Problem6();
+		Problem1.problem1();
+		Problem2.Problem2();
+		Problem3.Problem3();
+	    Problem4.Problem4();
+	 	Problem5.Problem5();
+	    Problem6.Problem6();
 		Problem7.Problem7();
 	}
 
